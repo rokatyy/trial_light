85170f809136c3f5cbbadd3ee11b07f36d7bf3b7
Ekaterina <ekaterina.rogatova@dell.com>
2019-09-25 11:41:15 +0300

    Migrate sqlite to postgres

--- trial_light.py
+++ trial_light.py
@@ -31,12 +31,9 @@ class Database:
 
 class User:
     def __init__(self, name):
-        self.conn = sqlite3.connect(DATABASE_PATH)
-        self.cursor = conn.cursor()
         self.name = name
         self.clear_name()
-        user_exists = self.check_db()
-        if not user_exists: self.create_new_user()
+
 
     def clear_name(self):
         """
@@ -46,17 +43,11 @@ class User:
 
     def check_db(self):
         sql_query = 'select * from {table} where name = {name}'.format(table = TABLE_NAME, name = self.name)
-        try:
-            self.cursor.execute(sql_query)
-            result = self.cursor.fetchall()
-        except sqlite3.OperationalError as e:
-            return False
-        return (name in result) 
+
 
     def create_new_user(self):
         sql_query = 'insert into {table} values(\'name\', 5'.format(table = TABLE_NAME, name = self.name, access_count = ACCESS_COUNT_LIMIT)
-        self.cursor.execute(sql_query)
-        self.conn.commit()
+
      
     @staticmethod
     def authorization(): 
