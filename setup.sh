999611f30e7d7eca64dbdb3dcee4e6e10cf84ffc
rokatyy <erogatova@gmail.com>
2019-09-27 12:32:44 +0300

    new options

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-mkdir /tmp/trial_light/ && touch info.csv;
+mkdir /tmp/trial_light/ && touch /tmp/trial_light/info.csv;
+PWD=`pwd`
+echo "alias ssc='python3 $PWD/trial_light.py' " >> ~/.bashrc
+bash
+
+
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+sed -i '/alias ssc/d' ~/.bashrc
+bash
