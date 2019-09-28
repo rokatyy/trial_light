e5c6957802a12dddeb275e6dcfc4ab674c0fbe6a
rokatyy <erogatova@gmail.com>
2019-09-28 09:15:52 +0300

    Formatted bash scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-mkdir /tmp/trial_light/ && touch /tmp/trial_light/info.csv;
-PWD=`pwd`
-echo "alias ssc='python3 $PWD/trial_light.py' " >> ~/.bashrc
+mkdir /tmp/trial_light/
+touch /tmp/trial_light/info.csv
+PWD=$(pwd)
+echo "alias ssc='python3 $PWD/trial_light.py' " >>~/.bashrc
 bash
-
-
